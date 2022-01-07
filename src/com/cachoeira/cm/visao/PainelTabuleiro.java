ce62041137b73814ed21ceefd546511ef626d308
Eduardo Cachoeira <epcachoeira@hotmail.com>
2022-01-07 16:22:21 -0300

    Implementação da Interface Gráfica #01

---
+++ src/com/cachoeira/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,13 @@
+package com.cachoeira.cm.visao;
+
+import javax.swing.JPanel;
+
+import com.cachoeira.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+}
